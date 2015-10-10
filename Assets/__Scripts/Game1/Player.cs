b05b55071a201f537e96335dcb97d2c61f753303
孤月蓝风 <trlanfeng@foxmail.com>
2015-10-10 17:38:47 +0800

    准备加入平衡木小游戏

---
+++ Assets/__Scripts/Game1/Player.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.W))
+        {
+            this.GetComponent<Rigidbody>().velocity = Vector3.forward * 10;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            this.GetComponent<Rigidbody>().velocity = (Vector3.back * 10);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            this.GetComponent<Rigidbody>().velocity = (Vector3.left * 10);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            this.GetComponent<Rigidbody>().velocity = (Vector3.right * 10);
+        }
+    }
+}
