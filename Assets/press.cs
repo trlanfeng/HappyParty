288c535a7f674aed057928dd2553c09468d837a7
孤月蓝风 <trlanfeng@foxmail.com>
2015-10-28 18:59:26 +0800

    加入气球游戏

---
+++ Assets/press.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class press : MonoBehaviour {
+
+    Rigidbody2D r2d;
+
+	// Use this for initialization
+	void Start () {
+        r2d = transform.GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            r2d.AddForce(new Vector2(-10, 30));
+        }
+        if (Input.GetMouseButtonDown(1))
+        {
+            r2d.AddForce(new Vector2(10, 30));
+        }
+        if (r2d.velocity.x > 10)
+        {
+            r2d.velocity = new Vector2(10, r2d.velocity.y);
+        }
+        if (r2d.velocity.y > 30)
+        {
+            r2d.velocity = new Vector2(r2d.velocity.x,30);
+        }
+	}
+}
